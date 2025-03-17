82780c43f4959dda1761359dd7e45627f772fd68
Aykut Sevim <aykutsevim@hotmail.com>
2025-03-18 01:31:20 +0300

    initial commit

---
+++ components/lvgl__lvgl/demos/tea_timer/lv_tea_timer.h
@@ -0,0 +1,40 @@
+/**
+ * @file lv_tea_timer.h
+ *
+ */
+
+#ifndef LV_TEA_TIMER_H
+#define LV_TEA_TIMER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "../lv_demos.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+void lv_tea_timer(void);
+void lv_tea_timer_close(void);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /*LV_TEA_TIMER_H*/
